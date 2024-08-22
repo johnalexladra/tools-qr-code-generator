0fe22362dd068d3eec0a6e96534cbffb074e6d26
John Alex Ladra <johnalexladra@gmail.com>
2024-08-22 12:26:06 +0800

    Renamed file to index and added a comment

---
+++ index.py
@@ -0,0 +1,24 @@
+import qrcode
+
+# Data to encode
+data = "https://www.example.com"
+
+# Create a QR code object
+qr = qrcode.QRCode(
+    version=1,  # Controls the size of the QR Code
+    error_correction=qrcode.constants.ERROR_CORRECT_L,  # Controls the error correction used for the QR Code
+    box_size=10,  # Controls how many pixels each box of the QR code is
+    border=4,  # Controls how many boxes thick the border should be
+)
+
+# Add data to the QR code object
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create an image from the QR Code instance
+img = qr.make_image(fill='black', back_color='white')
+
+# Save the image
+img.save("qrcode.png")
+
+print("QR code generated and saved as 'qrcode.png'")
